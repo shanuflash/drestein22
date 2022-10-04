fce6f4ad341ecceec0f0437405a8b10cc6c01159
Shanu Flash <shanuflash@gmail.com>
2022-10-04 23:11:40 +0530

    limit particles, fix lag

--- src/Particalobj.js
+++ src/Particalobj.js
@@ -2,7 +2,7 @@ export const Particalobj = {
   fpsLimit: 8,
   particles: {
     number: {
-      value: 355,
+      value: 175,
       density: {
         enable: true,
         value_area: 789.1476416322727,
@@ -70,74 +70,3 @@ export const Particalobj = {
   },
   retina_detect: true,
 };
-
-// const Oldpartical = {
-//   background: {
-//     color: {
-//       value: "#000",
-//     },
-//   },
-//   fpsLimit: 20,
-//   interactivity: {
-//     events: {
-//       onClick: {
-//         enable: true,
-//         mode: "push",
-//       },
-//       onHover: {
-//         enable: true,
-//         mode: "repulse",
-//         parallax: { enable: false, force: 60, smooth: 50 },
-//       },
-//       resize: true,
-//     },
-//     modes: {
-//       push: { quantity: 4 },
-//       repulse: {
-//         distance: 200,
-//         duration: 0.4,
-//       },
-//     },
-//   },
-//   particles: {
-//     color: {
-//       value: "#ffffff",
-//     },
-
-//     move: {
-//       direction: "none",
-//       enable: true,
-//       outModes: "out",
-//       random: false,
-//       speed: 2,
-//       straight: false,
-//     },
-//     number: {
-//       density: {
-//         enable: true,
-//         area: 800,
-//       },
-//       value: 50,
-//     },
-//     opacity: {
-//       animation: {
-//         enable: true,
-//         speed: 0.05,
-//         sync: true,
-//         startValue: "max",
-//         count: 1,
-//         destroy: "min",
-//       },
-//       value: {
-//         min: 0,
-//         max: 0.5,
-//       },
-//     },
-//     shape: {
-//       type: "star",
-//     },
-//     size: {
-//       value: { min: 1, max: 5 },
-//     },
-//   },
-// };
