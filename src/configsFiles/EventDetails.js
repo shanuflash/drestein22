b50f9cce4e4e15e3d8f1f32959346f4f2f87ca70
Shanu S <shanuflash@gmail.com>
2022-10-16 23:41:23 +0530

    fixed events mapping

--- src/configsFiles/EventDetails.js
+++ src/configsFiles/EventDetails.js
@@ -1,7 +1,7 @@
-export const EventsDetails = [
+export const product = [
   {
     title: "Information Technology",
-    events: [
+    eventslist: [
       {
         name: "Event 1",
         desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, totam?",
@@ -48,53 +48,50 @@ export const EventsDetails = [
   },
   {
     title: "Computer Science & Engineering",
-    events: [
+    eventslist: [
       {
-        name: "Event 1test",
+        name: "Event 1",
         desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, totam?",
         date: "10 Nov 2022",
         tag: "EVENT",
         img: "https://www.gstatic.com/mobilesdk/191021_mobilesdk/discoverycards/appdistro@2x.png",
       },
       {
-        name: "Event 2test",
+        name: "Event 2",
         desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, totam?",
         date: "10 Nov 2022",
         tag: "EVENT",
         img: "https://www.gstatic.com/mobilesdk/191021_mobilesdk/discoverycards/appdistro@2x.png",
       },
       {
-        name: "Event 3test",
+        name: "Event 3",
         desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, totam?",
         date: "11 Nov 2022",
         tag: "EVENT",
         img: "https://www.gstatic.com/mobilesdk/191021_mobilesdk/discoverycards/appdistro@2x.png",
       },
       {
-        name: "Event 4test",
+        name: "Event 4",
         desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, totam?",
         date: "11 Nov 2022",
         tag: "EVENT",
         img: "https://www.gstatic.com/mobilesdk/191021_mobilesdk/discoverycards/appdistro@2x.png",
       },
       {
-        name: "Workshop 1test",
+        name: "Workshop 1",
         desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, totam?",
         tag: "WORKSHOP",
         date: "10 Nov 2022",
         img: "https://codetheweb.blog/assets/img/posts/html-syntax/cover.jpg",
       },
       {
-        name: "Workshop 2test",
+        name: "Workshop 2",
         desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, totam?",
         tag: "WORKSHOP",
         date: "11 Nov 2022",
         img: "https://codetheweb.blog/assets/img/posts/html-syntax/cover.jpg",
       },
     ],
-  },
-  {
-    title: "Test",
   },
 ];
 
--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,31 +1,26 @@
 import React from "react";
 import Nav from "../component/Nav";
 import "../styles/events.css";
-import { EventsDetails } from "../configsFiles/EventDetails";
+import { product } from "../configsFiles/EventDetails";
 import EventCard from "../component/EventCard";
-// import EventDepartment from "../component/EventDepartment";
 
 const Events = () => {
   return (
     <div className="event-page">
       <Nav />
       <div className="event-container">
-        {EventsDetails.map((EachDept) => {
-          return (
-            <>
-              <div className="top">
-                <div className="title">{EachDept.title}</div>
-              </div>
-              <div className="wrapper">
-                {EventsDetails.map((EachEvent) => {
-                  console.log(EachEvent);
-                  return <EventCard {...EachEvent.events} />;
-                })}
-              </div>
-            </>
-          );
-          // return <EventDepartment title={EachDept.title} />;
-        })}
+        {product.map((data) => (
+          <>
+            <div className="top">
+              <div className="title">{data.title}</div>
+            </div>
+            <div className="wrapper">
+              {data.eventslist.map((datas) => (
+                <EventCard {...datas} />
+              ))}
+            </div>
+          </>
+        ))}
       </div>
     </div>
   );
