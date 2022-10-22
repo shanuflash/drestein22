5a42bb6d7628d34f7f1cab9fe1164d9cd4612ca5
Shanu S <shanuflash@gmail.com>
2022-10-22 14:53:52 +0530

    fix timer

--- src/components/Header/Countdown.jsx
+++ src/components/Header/Countdown.jsx
@@ -20,19 +20,23 @@ const CountDown = () => {
         const msDiff = end.getTime() - new Date().getTime();
         const daysDiff = Math.trunc(msDiff / (1000 * 3600 * 24));
         setdaysDiff(daysDiff);
-        sethoursDiff(23 - new Date().getHours());
-        setminutesDiff(59 - new Date().getMinutes());
-        setsecondsDiff(59 - new Date().getSeconds());
+        {
+          hoursDiff > 10 || hoursDiff === "00"
+            ? sethoursDiff(23 - new Date().getHours())
+            : sethoursDiff("0" + (23 - new Date().getHours()));
+        }
+        {
+          minutesDiff > 10 || minutesDiff === "00"
+            ? setminutesDiff(59 - new Date().getMinutes())
+            : setminutesDiff("0" + (59 - new Date().getMinutes()));
+        }
+
+        {
+          secondsDiff > 10 || secondsDiff === "00"
+            ? setsecondsDiff(59 - new Date().getSeconds())
+            : setsecondsDiff("0" + (59 - new Date().getSeconds()));
+        }
       }, 1000);
-      if (hoursDiff < 10) {
-        sethoursDiff("0" + (59 - new Date().getHours()));
-      }
-      if (minutesDiff < 10) {
-        setminutesDiff("0" + (59 - new Date().getMinutes()));
-      }
-      if (secondsDiff < 10) {
-        setsecondsDiff("0" + (59 - new Date().getSeconds()));
-      }
       return () => {
         clearInterval(timer);
       };
