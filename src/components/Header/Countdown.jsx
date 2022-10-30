9e3ecab77e1b698694743d3759f4056430ff80ff
Shanu S <shanuflash@gmail.com>
2022-10-30 16:04:56 +0530

    fix

--- src/components/Header/Countdown.jsx
+++ src/components/Header/Countdown.jsx
@@ -20,22 +20,9 @@ const CountDown = () => {
         const msDiff = end.getTime() - new Date().getTime();
         const daysDiff = Math.trunc(msDiff / (1000 * 3600 * 24));
         setdaysDiff(daysDiff);
-        {
-          hoursDiff > 10 || hoursDiff === "00"
-            ? sethoursDiff(23 - new Date().getHours())
-            : sethoursDiff("0" + (23 - new Date().getHours()));
-        }
-        {
-          minutesDiff > 10 || minutesDiff === "00"
-            ? setminutesDiff(59 - new Date().getMinutes())
-            : setminutesDiff("0" + (59 - new Date().getMinutes()));
-        }
-
-        {
-          secondsDiff > 10 || secondsDiff === "00"
-            ? setsecondsDiff(59 - new Date().getSeconds())
-            : setsecondsDiff("0" + (59 - new Date().getSeconds()));
-        }
+        sethoursDiff(23 - new Date().getHours());
+        setminutesDiff(59 - new Date().getMinutes());
+        setsecondsDiff(59 - new Date().getSeconds());
       }, 1000);
       return () => {
         clearInterval(timer);
