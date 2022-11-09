c05a8a29a0ba3c41713665e2d052a6fb4a62c101
bharath <bharath18117886@gmail.com>
2022-11-09 10:30:23 +0530

    updated

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,9 +1,49 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./counter.css";
 import CountUp from "react-countup";
 import VisibilitySensor from "react-visibility-sensor";
+import { collection, doc, getDocs } from "firebase/firestore";
+import { onSnapshot } from "firebase/firestore";
+import { db } from "../../configs/Firebase.config";
 const Counter = ({ ...rest }) => {
   const [viewPortEntered, setViewPortEntered] = useState(false);
+const [users,setUsers] = useState(300)
+
+useEffect(()=>{
+  functionfetch()
+},[])
+    const docRef = collection(db,'RegisteredPeople')
+
+    const colref = collection(db, "RegisteredPeople");
+    // const q = query(colref, where("cashPaid", "==", false));
+  
+    
+
+     const promice =  new Promise((resolve,reject)=>{
+        onSnapshot(colref, (snapshot) => {
+          let books = [];
+          console.log(snapshot.docs);
+          snapshot.docs.forEach((doc) => {
+            books.push({ ...doc.data(), id: doc.id });
+          });
+          // console.log(books)
+          console.log(books);
+          // setUsers(pre=>pre+books.length);
+          resolve(books)
+    
+        });
+      })
+const functionfetch = async()=>{
+  const response  = await promice
+
+  console.log(response.length)
+  setUsers(300+response.length)
+
+
+}
+    
+
+
   return (
     <div className="counter">
       <div className="counter-row">
@@ -75,7 +115,7 @@ const Counter = ({ ...rest }) => {
         </div>
         <div className="counter-column">
           <strong data-number="305">
-            <CountUp {...rest} start={viewPortEntered ? null : 0} end={305}>
+            <CountUp {...rest} start={viewPortEntered ? null : 0} end={users}>
               {({ countUpRef }) => {
                 return (
                   <VisibilitySensor
@@ -92,7 +132,7 @@ const Counter = ({ ...rest }) => {
             </CountUp>
           </strong>
           <span>
-            On Going <br /> Projects{" "}
+            Registered <br /> people{" "}
           </span>
         </div>
       </div>
