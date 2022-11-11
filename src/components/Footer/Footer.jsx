065ba8061cbf618ccc98e3d809521222dfb2c029
bharath <bharath18117886@gmail.com>
2022-11-11 16:13:03 +0530

    cse updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,8 +12,18 @@ import {
   FaInstagram,
   FaPinterest,
 } from "react-icons/fa";
-
+import { useContext } from "react";
+import { UserContext } from "../Admin/contexts/AdminContext";
+import { useEffect } from "react";
+import { useState } from "react";
 const Footer = () => {
+  const {vicitedProple}  = useContext(UserContext)
+  const [views,setViews] = useState(vicitedProple)
+
+  useEffect(()=>{
+    setViews(vicitedProple)
+  },[vicitedProple])
+
   const LogoHead = styled.div`
     display: flex;
     width: fit-content;
@@ -72,6 +82,9 @@ const Footer = () => {
           <div className="desc">
             13<sup>th</sup> National level inter collegiate technical and
             management fest
+          </div>
+          <div>
+             {vicitedProple}
           </div>
         </div>
         <div className="block">
